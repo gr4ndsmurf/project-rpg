98f7dcc6e1cb533f3fdcf41485acf105fdd43986
Akın Haydaroğlu <gr4ndsmurf@gmail.com>
2024-02-11 04:52:49 +0300

    Interaction bugfix

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -32,7 +32,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButtonDown(1))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
